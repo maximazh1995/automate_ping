6f5db6c1a3c38fad09f9934ca3c35f440197a13d
maximazh@gmail.com <Zrjdktd2domains>
2021-11-14 06:30:50 +0300

    Добавлена progressbar (для визуального отображения выполнения программы). Примечание: продвигается progressbar только после завешения попытки.

--- automate_ping/automate_ping.py
+++ automate_ping/automate_ping.py
@@ -6,7 +6,7 @@ import click
 
 def ping_server(target_ip, package_size, package_count):
     count_of_success_packages = 0
-    response_list = ping(target_ip, size=package_size, count=package_count, verbose = True)
+    response_list = ping(target_ip, size=package_size, count=package_count)
     for response in response_list:
         if response.success:
             count_of_success_packages += 1
@@ -17,6 +17,17 @@ def ping_server(target_ip, package_size, package_count):
 @click.argument('target_ip')
 @click.argument('package_size')
 def main(target_ip, package_size):
+    """
+    Скрипт для отправки icmp запросов на сервер: \n
+        - количество попыток = 3 \n
+        - количество icmp пакетов в одну попытку = 120 \n
+        - интервал между попытками в секундах = 5 сек \n
+
+    Обязательные аргументы: \n
+        - target_ip: целевой адрес \n
+        - package_size: размер пакета (без учета заголовка ICMP - 8 байт) \n
+
+    """
     # target_ip = '8.8.8.8'  # целевой адрес
     # package_size = 60  # размер пакета (без учета заголовка ICMP - 8 байт)
     package_count = 20  # количество icmp пакетов в одну попытку
@@ -24,9 +35,11 @@ def main(target_ip, package_size):
     success = [0] * number_of_attempts  # массив для хранения количества успешных попыток
     interval = 5  # интервал между попытками в секундах
 
-    for i in range(0, number_of_attempts):
-        success[i] = ping_server(target_ip, int(package_size), package_count)
-        time.sleep(interval)
+    with click.progressbar(length=number_of_attempts, label=f'Running') as bar:
+        for i in range(0, number_of_attempts):
+            success[i] = ping_server(target_ip, int(package_size), package_count)
+            time.sleep(interval)
+            bar.update(i)
 
     print(f"Среднее за {number_of_attempts} попыток(ки) успешных пингов = {numpy.mean(success) * 100 / package_count}%")
 

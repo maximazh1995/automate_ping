2d79b529e173c06ee07aa493ce5100dc6eaf36fc
maximazh@gmail.com <Zrjdktd2domains>
2021-11-14 06:26:18 +0300

    Добавлена возможность обязательного ввода аргументов (целевого ip адреса   (target_ip) и размер пакета (package_size))

--- automate_ping/automate_ping.py
+++ automate_ping/automate_ping.py
@@ -13,16 +13,19 @@ def ping_server(target_ip, package_size, package_count):
     return count_of_success_packages
 
 
-def main():
-    target_ip = '8.8.8.8'  # целевой адрес
-    package_size = 60  # размер пакета (без учета заголовка ICMP - 8 байт)
+@click.command()
+@click.argument('target_ip')
+@click.argument('package_size')
+def main(target_ip, package_size):
+    # target_ip = '8.8.8.8'  # целевой адрес
+    # package_size = 60  # размер пакета (без учета заголовка ICMP - 8 байт)
     package_count = 20  # количество icmp пакетов в одну попытку
     number_of_attempts = 3  # количество попыток
     success = [0] * number_of_attempts  # массив для хранения количества успешных попыток
     interval = 5  # интервал между попытками в секундах
 
     for i in range(0, number_of_attempts):
-        success[i] = ping_server(target_ip, package_size, package_count)
+        success[i] = ping_server(target_ip, int(package_size), package_count)
         time.sleep(interval)
 
     print(f"Среднее за {number_of_attempts} попыток(ки) успешных пингов = {numpy.mean(success) * 100 / package_count}%")
